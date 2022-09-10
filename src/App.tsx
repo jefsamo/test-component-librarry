af66b5812d29d6b12f8874b9274df6a6339d50c1
Olawale Shopeyin <shopeyinwale@gmail.com>
2022-09-10 06:54:18 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "component-library";
 
-function App() {
+export interface IApplicationProps {}
+
+const App: React.FC<IApplicationProps> = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button backgroundColor="green" color="white" style={{ padding: "20px" }}>
+        Cool
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
